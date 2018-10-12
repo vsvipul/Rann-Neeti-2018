fc153476c39641e51cee0aed7a86c73332fcc05c
nimradium <nimra242001@gmail.com>
2018-10-12 18:53:24 +0200

    New team page (js/json)
    ++ css/team/team_old.css:   * css file for old team page
    
    ++ team.html:               * new team page
    
    ++ team.json:               * file including all team members
    
    ++ js/team.js:              * script to add the team members to the page
    
    -- team.php:                * php doesn't work with github pages --> javascript
    
    -- team.xml:                * json is easier to use with javascript than xml
    
    Changes:
    
    > css/team/team.css:        + new CSS for team page
    
    > index.html:               bugfix
    
    > team_old.html:            change to old css

---
+++ js/team.js
@@ -0,0 +1,48 @@
+(function ($) {
+
+    function loadFile(callback) {
+        var req = new XMLHttpRequest();
+
+        req.onreadystatechange = function () {
+            if (req.readyState == 4) {
+                callback(req.response);
+            }
+        };
+        req.open("GET", "team.json", true);
+        req.send();
+    }
+
+    var dataProcessor = function (response) {
+        
+        var data = JSON.parse(response);
+        console.log(data);
+        var members = data.team.member;
+        
+        members.forEach(function (member) {
+            var category = member.category,
+                id = member.id,
+                image = "<div class='image'><img src='images/team/" + member.image + "'/></div>",
+                name = "<a class='name'>" + member.name + "</a>",
+                role = "<a class='role'>" + member.role + "</a>",
+                tel = "<a href='tel:" + member.tel + "'>" + member.tel + "</a>";
+
+
+            var fb = "<a href='" + member.link.fb +  "' target='_blank'><i class='fa fa-facebook'></i></a>";
+            
+            if (member.link.gh != null) {
+                var gh = "<a href='" + member.link.gh + "' target='_blank'><i class='fa fa-github'></i></a>";
+                
+                var contact = tel + "<br><br>" + fb + gh;
+            }else{
+                var contact = tel + "<br><br>" + fb;
+            }
+
+
+            var html = $("<div class='team-member'>" + image + "<div class='label'><div class='label-text'>" + name + role + "<p class='contact'>" + contact + "</p></div><div class='label-bg'></div></div></div>");
+            
+            $('#team').append(html);
+        })
+    };
+
+    loadFile(dataProcessor);
+})(jQuery);
